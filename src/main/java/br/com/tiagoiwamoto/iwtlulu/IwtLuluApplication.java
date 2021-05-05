ce3fc44611245e4f1bcd2489887de88a99a204c2
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2021-05-05 20:09:17 -0300

    Atualização do projeto lulu

---
+++ src/main/java/br/com/tiagoiwamoto/iwtlulu/IwtLuluApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagoiwamoto.iwtlulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwtLuluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwtLuluApplication.class, args);
+    }
+
+}
