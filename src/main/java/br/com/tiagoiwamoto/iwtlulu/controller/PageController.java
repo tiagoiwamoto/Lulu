08d3ab7b117726bd024ebe72db49c1d9e8313850
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2021-05-11 15:22:39 -0300

    Ajuste de cadastro, validação, efeito, snackbar, listagem de medicações

--- src/main/java/br/com/tiagoiwamoto/iwtlulu/controller/PageController.java
+++ src/main/java/br/com/tiagoiwamoto/iwtlulu/controller/PageController.java
@@ -8,12 +8,22 @@ package br.com.tiagoiwamoto.iwtlulu.controller;
  * 05/05/2021 | 21:01
  */
 
+import br.com.tiagoiwamoto.iwtlulu.business.object.MedicationBO;
+import br.com.tiagoiwamoto.iwtlulu.business.object.UserBO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
+
+    private final UserBO userBO;
+    private final MedicationBO medicationBO;
+
+    public PageController(UserBO userBO, MedicationBO medicationBO) {
+        this.userBO = userBO;
+        this.medicationBO = medicationBO;
+    }
 
     @GetMapping(path = "/")
     public ModelAndView index(){
